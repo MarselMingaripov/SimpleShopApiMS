583ebf60e6dfe1c9e7e943762c94eee77540ba03
MarselMingaripov <bard1812@gmail.com>
2023-03-18 11:12:42 +0300

    init commit

---
+++ src/main/java/ru/min/simleshopapims/SimleShopApiMsApplication.java
@@ -0,0 +1,13 @@
+package ru.min.simleshopapims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimleShopApiMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimleShopApiMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/min/simleshopapims/SimleShopApiMsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.min.simleshopapims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimleShopApiMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
